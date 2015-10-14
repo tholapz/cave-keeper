571ccf056558d41106bae9ecd5bd4fb5bac543f7
tholapz <tholapz@gmail.com>
2015-10-14 02:06:46 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+var React = require('react');
+var CaveKeeperApp = require('./components/CaveKeeperApp.react');
+
+React.render(
+    <CaveKeeperApp />,
+    document.getElementById('react')
+);
